62a548d7befbce01bf28e1babfbbd8e46fa163ed
Randi Schreiner <mimirandi21@gmail.com>
2021-01-12 18:06:35 -0600

    commit tree 954de95e1dacc47a9507010e9c9daef5a748fcd0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
+  return driver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let newdriver = Object.assign({}, driver);
+  delete newdriver[key];
+  return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
